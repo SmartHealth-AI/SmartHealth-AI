e4d0417b13886c88023e893e2f6a20b7033f2338
SeanseaNG21 <nguyenviethung21092003@gmail.com>
2024-10-30 17:41:56 +0700

    Feature: Create Authentification

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class User(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  # uuid
+  # created_by
+  # created_at
+  # updated_by
+  # updated_at
+
+  def __str__(self):
+    return str(self.firstname) + str(self.lastname)
