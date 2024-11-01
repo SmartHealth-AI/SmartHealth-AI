83d712297a94b59d93a361373f67afe49c7d7c98
SeanseaNG21 <nguyenviethung21092003@gmail.com>
2024-11-01 11:19:51 +0700

    Test: add functional test for user service

--- backend/authentification/views.py
+++ backend/authentification/views.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
-#from .serializers import RegisterSerializer, LoginSerializer
 from django.contrib.auth import authenticate
 from django.conf import settings
 from rest_framework import exceptions as rest_exceptions
@@ -19,40 +17,6 @@ class HomeView(APIView):
        content = {'message': 'JWT Authentication page'}
        return Response(content)
 
-
-# class LogoutView(APIView):
-#      permission_classes = (IsAuthenticated,)
-#      def post(self, request):
-          
-#           try:
-#                refresh_token = request.data["refresh_token"]
-#                token = RefreshToken(refresh_token)
-#                token.blacklist()
-#                return Response(status=status.HTTP_205_RESET_CONTENT)
-#           except Exception as e:
-#                return Response(status=status.HTTP_400_BAD_REQUEST) 
-
-# class RegisterView(APIView):
-#     def post(self, request):
-#         serializer = RegisterSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# class LoginView(APIView):
-#     def post(self, request):
-#         serializer = LoginSerializer(data=request.data)
-#         if serializer.is_valid():
-#             user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
-#             if user:
-#                 refresh = RefreshToken.for_user(user)
-#                 return Response({
-#                     'refresh': str(refresh),
-#                     'access': str(refresh.access_token),
-#                 }, status=status.HTTP_200_OK)
-#             return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def get_user_tokens(user):
     refresh = tokens.RefreshToken.for_user(user)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,7 @@ from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('users.urls')),
-    path('auth/', include('authentification.urls')),
+    path('', include('authentification.urls')),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
     
